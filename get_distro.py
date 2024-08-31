3e15ecea079825a04bfde5da1ac713804892d7bd
Markku Leiniö <25136274+markkuleinio@users.noreply.github.com>
2024-08-31 13:45:38 +0300

    Use f-strings, changes for Debian testing to work

--- get_distro.py
+++ get_distro.py
@@ -1,3 +1,4 @@
+# Python 3.6+ required (f-strings)
 import csv
 
 RELEASE_DATA = {}
@@ -8,13 +9,28 @@ with open("/etc/os-release") as f:
         if row:
             RELEASE_DATA[row[0]] = row[1]
 
+try:
+    name = RELEASE_DATA["NAME"]
+except KeyError:
+    name = "Unknown"
+try:
+    version = RELEASE_DATA["VERSION"]
+except KeyError:
+    version = "(unknown version)"
+
 if RELEASE_DATA["ID"] in ["debian", "raspbian"]:
     with open("/etc/debian_version") as f:
         DEBIAN_VERSION = f.readline().strip()
-    major_version = DEBIAN_VERSION.split(".")[0]
-    version_split = RELEASE_DATA["VERSION"].split(" ", maxsplit=1)
-    if version_split[0] == major_version:
-        # Just major version shown, replace it with the full version
-        RELEASE_DATA["VERSION"] = " ".join([DEBIAN_VERSION] + version_split[1:])
+    try:
+        if "." not in DEBIAN_VERSION:
+            raise ValueError    # Let the except clause handle
+        major_version = DEBIAN_VERSION.split(".")[0]
+        version_split = version.split(" ", maxsplit=1)
+        if version_split[0] == major_version:
+            # Just major version was shown in /etc/os-release, use the full version
+            version = f"{DEBIAN_VERSION} {version_split[1]}"
+    except:
+        # Something went wrong ("xxx/sid"?), just use the info from /etc/debian_version
+        version = DEBIAN_VERSION
 
-print("{} {}".format(RELEASE_DATA["NAME"], RELEASE_DATA["VERSION"]))
+print(f"{name} {version}")
